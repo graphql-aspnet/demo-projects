05a011c4f0e1043370072e2edcb09f9c73092b96
Kevin Carroll <kevin.carroll@outlook.com>
2021-07-11 06:57:59 -0700

    Updated demo projects to v0.6.1-beta (#6)

--- Subscriptions-AzureServiceBus/src/Subscriptions-Server/Startup.cs
+++ Subscriptions-AzureServiceBus/src/Subscriptions-Server/Startup.cs
@@ -1,25 +1,16 @@
 namespace GraphQL.AspNet.Examples.Subscriptions.SubscriptionServer
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
+    using System.Diagnostics;
+    using GraphQL.AspNet.AzureServiceBus;
+    using GraphQL.AspNet.Configuration.Mvc;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using GraphQL.AspNet.Configuration.Mvc;
-    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.WebSockets;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Logging;
-    using GraphQL.AspNet.Interfaces.Subscriptions;
-    using GraphQL.AspNet.AzureServiceBus;
-    using GraphQL.AspNet.Configuration.Exceptions;
-    using GraphQL.AspNet.AzureServiceBus.Serializers;
-    using System.Diagnostics;
-    using GraphQL.AspNet.Examples.Subscriptions.DataModel;
     using Subscriptions_Server.Controllers;
-    using Microsoft.AspNetCore.WebSockets;
 
     public class Startup
     {
