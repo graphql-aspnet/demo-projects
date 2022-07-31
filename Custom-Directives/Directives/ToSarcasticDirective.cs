f9b42fb4a7469b5d9cc1d452c2b20fd0946061c4
Kevin Carroll <kevin.carroll@outlook.com>
2022-07-31 14:07:16 -0700

    fixes for 0.11.0 updates. Reworked directives project for new exe dir

--- Custom-Directives/Directives/ToSarcasticDirective.cs
+++ Custom-Directives/Directives/ToSarcasticDirective.cs
@@ -2,10 +2,14 @@
 {
     using GraphQL.AspNet.Attributes;
     using GraphQL.AspNet.Directives;
+    using GraphQL.AspNet.Execution.Contexts;
     using GraphQL.AspNet.Interfaces.Controllers;
+    using GraphQL.AspNet.Interfaces.PlanGeneration.DocumentParts;
     using GraphQL.AspNet.Schemas.TypeSystem;
     using System;
     using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     /// <summary>
     /// An execution directive that can be applied to any field in query document.
@@ -14,42 +18,46 @@
     /// </summary>
     public class ToSarcasticDirective : GraphDirective
     {
-        /// <summary>
-        /// This execution directive can only be applied directly to a field
-        /// in a query document.
-        /// </summary>
         [DirectiveLocations(DirectiveLocation.FIELD)]
         public IGraphActionResult Execute(bool startOnLowerCase = true)
         {
-            // for directives executing during query execution
-            // DirectiveTarget will be the resolved field value
-            //
-            // This resolved field value may be a scalar or enum for leaf fields
-            // or an object/scalar refrence for intermediate fields
-            //
-            if (this.DirectiveTarget != null && this.DirectiveTarget is string)
+            var fieldPart = this.DirectiveTarget as IFieldDocumentPart;
+            if (fieldPart != null)
             {
-                var builder = new StringBuilder();
-                var data = this.DirectiveTarget.ToString();
+                if (fieldPart.Field?.ObjectType != typeof(string))
+                    throw new Exception("ONLY STRINGS!"); // - hulk
 
-                var oddOrEven = startOnLowerCase ? 0 : 1;
-
-                for (var i = 0; i < data.Length; i++)
-                {
-                    if ((i % 2) == oddOrEven)
-                        builder.Append(data[i].ToString().ToLowerInvariant());
-                    else
-                        builder.Append(data[i].ToString().ToUpperInvariant());
-                }
-
-                // once the string is converted
-                // replace the resolved value with the new string
-                this.DirectiveTarget = builder.ToString();
+                // extend the resolver to include sarcastic casing
+                fieldPart.PostProcessor = CreateResolver(startOnLowerCase);
             }
 
-            // indicate that this directive completed
-            // successfully
             return this.Ok();
+        }
+
+        private static Func<FieldResolutionContext, CancellationToken, Task> CreateResolver(bool startOnLowerCase)
+        {
+            return (FieldResolutionContext context, CancellationToken token) =>
+            {
+                if (context.Result != null && context.Result is string)
+                {
+                    // for a single string leaf field convert and return
+                    var data = context.Result.ToString();
+                    var builder = new StringBuilder();
+
+                    var oddOrEven = startOnLowerCase ? 0 : 1;
+                    for (var i = 0; i < data.Length; i++)
+                    {
+                        if ((i % 2) == oddOrEven)
+                            builder.Append(data[i].ToString().ToLowerInvariant());
+                        else
+                            builder.Append(data[i].ToString().ToUpperInvariant());
+                    }
+
+                    context.Result = builder.ToString();
+                }
+
+                return Task.CompletedTask;
+            };
         }
     }
 }
